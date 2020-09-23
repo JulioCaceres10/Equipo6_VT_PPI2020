9f841f09ceb74180cbe725cbf28128d00f86f094
julio caceres <juliocace21@gmail.com>
2020-09-23 19:16:29 +0000

    creacion de carpeta frontend y backend

---
+++ backend-crea/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/",function(req,res) {
+  res.send("mi primer servidor")
+})
+
+app.get("/chat",function(req,res) {
+  res.send("aqui te aclaramos tus dudas")
+})
+
+
+
+app.listen(8080,function(){
+  console.log("Servidor escuchando en el puerto 8080")
+})
